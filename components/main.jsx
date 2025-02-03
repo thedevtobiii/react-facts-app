3df6042aff59549aa143860534db6a0d0acfeab1
DevTobi <fariuoyetola11@gmail.com>
2025-02-03 23:17:48 +0100

    added more styling

--- components/main.jsx
+++ components/main.jsx
@@ -1,7 +1,16 @@
 export default function Main(){
  return(
   <>
-   <h1>Main component goes here</h1>
+  <main>
+    <h1>Fun Facts about React</h1>
+    <ul className="facts-list">
+      <li>Was first released in 2013</li>
+      <li>Was originally created Jordan Walke</li>
+      <li>Has over 100k stars on GitHub</li>
+      <li>is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps including mobile apps</li>
+    </ul>
+  </main>
   </>
  )
 }

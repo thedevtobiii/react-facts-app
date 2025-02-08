e554d23ff5da04ebd24156eacc9058d61020d97f
DevTobi <fariuoyetola11@gmail.com>
2025-02-08 22:17:27 +0100

    fixed logo not showing

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,9 +1,10 @@
+import icon from '../src/img/React-icon.png'
 export default function Navbar (){
   return(
     <>
     <header>
       <nav>
-        <img className="nav-img" src="img/React-icon.png" alt="React Logo" />
+        <img className="nav-img" src={icon} alt="React Logo" />
         <span>ReactFacts</span>
       </nav>  
     </header>
